5077623cb03b3feabb7a42c7a21312fd651b7372
Eric Swann <eric.swann@q2ebanking.com>
2017-12-12 08:47:17 -0600

    Updated starting point for cart

--- DevCon.Server_Workshop/DevCon.Api/Command/Controllers/CartController.cs
+++ DevCon.Server_Workshop/DevCon.Api/Command/Controllers/CartController.cs
@@ -1,26 +1,33 @@
-//using System.Threading.Tasks;
-//using DevCon.Api.Command.Models;
-//using DevCon.Command.Services.Commands.CreateCart;
-//using Microsoft.AspNetCore.Mvc;
-//
-//namespace DevCon.Api.Command.Controllers
-//{
-//    [Route("command/carts")]
-//    public class CartController : Controller
-//    {
-//        private readonly ICreateCartHandler _createCartHandler;
-//
-//        public CartController(ICreateCartHandler createCartHandler)
-//        {
-//            _createCartHandler = createCartHandler;
-//        }
-//
-//        [HttpPost]
-//        public async Task<IActionResult> CreateCartAsync([FromBody]CreateCartRequest request)
-//        {
-//            var response = await _createCartHandler.ExecuteAsync(new CreateCartCommand(request.UserId));
-//            return Ok(new CreateCartResponse { CartId = response.AggregateId });
-//        }
-//
-//    }
-//}
+using System;
+using System.Threading.Tasks;
+using DevCon.Api.Command.Models;
+using Microsoft.AspNetCore.Mvc;
+// using DevCon.Command.Services.Commands.CreateCart;
+
+namespace DevCon.Api.Command.Controllers
+{
+   [Route("command/carts")]
+   public class CartController : Controller
+   {
+    //    private readonly ICreateCartHandler _createCartHandler;
+
+    //    public CartController(ICreateCartHandler createCartHandler)
+    //    {
+    //        _createCartHandler = createCartHandler;
+    //    }
+
+    //    [HttpPost]
+    //    public async Task<IActionResult> CreateCartAsync([FromBody]CreateCartRequest request)
+    //    {
+    //        var response = await _createCartHandler.ExecuteAsync(new CreateCartCommand(request.UserId));
+    //        return Ok(new CreateCartResponse { CartId = response.AggregateId });
+    //    }
+
+        [HttpPost]
+       public IActionResult CreateCartAsync([FromBody]CreateCartRequest request)
+       {
+           return Ok(new CreateCartResponse { CartId = Guid.Empty });
+       }
+
+   }
+}
